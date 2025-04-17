1f310b25ff9c2ae982d657c44e6c34974284765a
ShinYounHo <suh2963@gmail.com>
2025-04-17 12:26:13 +0900

    init

---
+++ src/test/kotlin/com/example/back250417/Back250417ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.back250417
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Back250417ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
